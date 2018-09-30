f883896a689e9d31e7ffd451e9b2f28a9e84f9c4
takedaxmasayuki <takeda_masayuki@crossware.co.jp>
2018-09-30 10:16:17 +0900

    fix stack overflow error

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,7 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
+  def factorial(i: BigInt, acc: BigInt): BigInt = if (i == 0) acc else factorial(i - 1, i * acc) 
 
-  println(factorial(10000))
+  println(factorial(10000, 1))
 }
